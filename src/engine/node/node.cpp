6b863f4ec31436438e75d3597f20259a489d7132
Chigozie Oduah <oduahchigozie46@gmail.com>
2026-05-27 11:05:04 +0100

    initial script sample setup

--- src/engine/node/node.cpp
+++ src/engine/node/node.cpp
@@ -1,6 +1,8 @@
 #include "node.hpp"
+
 #include <memory>
 #include <string>
+#include <dlfcn.h>
 
 void engine::node::Node::draw() const {
     for (const auto &ptr : children) {
@@ -10,8 +12,16 @@ void engine::node::Node::draw() const {
 
 std::unique_ptr<engine::node::Node>
 engine::node::Node::construct(toml::table &tbl) {
+    // typedef void (*RegisterFunc)(const char *, void (*)(void *));
+
     auto node = std::make_unique<engine::node::Node>();
     node->name = tbl["name"].value_or("");
+
+    auto s = tbl["script"].value<std::string>();
+    if (s.has_value()) {
+        node->script = dlopen(s->c_str(), RTLD_LAZY);
+    }
     return node;
 }
 
+engine::node::Node::~Node() { dlclose(script); }
--- src/engine/node/node.hpp
+++ src/engine/node/node.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
-#include <any>
 #include <toml++/toml.hpp>
 #include <vector>
 
 namespace engine {
 namespace node {
 class Node {
+    void *script = nullptr;
+    struct ScriptHelper {};
+
   public:
-      std::any script;
+    ~Node();
     Node *parent = nullptr;
     std::string name;
     std::vector<std::unique_ptr<node::Node>> children;
     void draw() const;
-    
+
     static std::unique_ptr<Node> construct(toml::table &);
 };
 } // namespace node
